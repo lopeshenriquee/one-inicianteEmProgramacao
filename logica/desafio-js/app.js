9b10e38efcb92f5aadb404eea55d9b478b3f2d38
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-19 11:43:49 -0300

    first commit: primeiros passos js

---
+++ logica/desafio-js/app.js
@@ -0,0 +1,14 @@
+/*
+let nomeDela = lua;
+let idadeDela = 25;
+let numeroDeVendas = 50;
+let saldoDisponível = 1000;
+*/
+//mensagemVerificacao = ("Preencha todos os Campos!!!");
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+if (idade >= 18) {
+    alert("Voê pode tirar a Habilitação")
+} else {
+    alert("Você ainda não pode tirar a Habilitação")
+}
---
+++ logica/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert("Boas vindas ao jogo!");
+//let é para declarar variável
+let numeroSecreto = 5;
+let chute = prompt("Escolha um n entre 1 e 10")
+
+if (chute == numeroSecreto ){
+    alert("Isso aí, você acertou!");
+}
+else {
+    alert("Você errou, que pena");
+}
