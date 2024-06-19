0a9e3005d988264ce2c66a9ea2989c2651dc0bc7
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-19 12:53:14 -0300

    commit: melhorando o codigo

--- logica/logica-js-projeto_inicial/app.js
+++ logica/logica-js-projeto_inicial/app.js
@@ -1,12 +1,32 @@
 alert("Boas vindas ao jogo!");
 //let é para declarar variável
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um n entre 1 e 10")
+console.log(numeroSecreto);
+let chute;
+let tentativa = 1;
 
-if (chute == numeroSecreto ){
-    alert(`Isso aí, você acertou! O número era: ${numeroSecreto}`);
+while (chute != numeroSecreto) {
+
+    chute = prompt("Escolha um número entre 1 e 10");
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        //alert('Você errou, o número era: ' + numeroSecreto + ' e você digitou: ' + chute);
+        if (chute > numeroSecreto) {
+            alert(`O número Secreto é menor que ${chute}`);
+        } else {
+            alert(`O número Secreto é maior que ${chute}`);
+        }
+        tentativa++;
+    }
 }
-else {
-    alert('Você errou, o número era: ' + numeroSecreto + ' e você digitou: ' + chute);
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí, você descobriu o número secreto ${numeroSecreto} com ${tentativa} ${palavraTentativa}!`);
+
+/*
+if(tentativa > 1){
+    alert(`Isso aí, você acertou  com ${tentativa} tentativas! O número era: ${numeroSecreto}`);
+} else {
+    alert(`Isso aí, você acertou! com ${tentativa} tentativa! O número era: ${numeroSecreto}`);
 }
+*/
