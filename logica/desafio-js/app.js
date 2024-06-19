bb0b88421f2decfa9ce6de6b1a18ae18189f2084
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-19 12:12:44 -0300

    commit: arrumando código

--- logica/desafio-js/app.js
+++ logica/desafio-js/app.js
@@ -8,7 +8,7 @@ let saldoDisponível = 1000;
 nome = prompt("Qual o seu nome?");
 idade = prompt("Qual a sua idade?");
 if (idade >= 18) {
-    alert("Voê pode tirar a Habilitação")
+    alert("Você pode tirar a Habilitação")
 } else {
     alert("Você ainda não pode tirar a Habilitação")
 }
--- logica/logica-js-projeto_inicial/app.js
+++ logica/logica-js-projeto_inicial/app.js
@@ -1,11 +1,12 @@
 alert("Boas vindas ao jogo!");
 //let é para declarar variável
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt("Escolha um n entre 1 e 10")
 
 if (chute == numeroSecreto ){
-    alert("Isso aí, você acertou!");
+    alert(`Isso aí, você acertou! O número era: ${numeroSecreto}`);
 }
 else {
-    alert("Você errou, que pena");
+    alert('Você errou, o número era: ' + numeroSecreto + ' e você digitou: ' + chute);
 }
