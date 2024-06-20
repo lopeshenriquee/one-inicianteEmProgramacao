ba51c2553dec2ff4fc1244ea29b5286d09221ca6
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-20 12:19:05 -0300

    commit: function com parametro

--- logica/js-curso-2/app.js
+++ logica/js-curso-2/app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Jogo do Número Secreto';
+// let titulo = document.querySelector('h1')
+// titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTexto(){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Jogo do Número Secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log('Botao foi clicado!')
+    console.log('Botao foi clicado!');
 }
