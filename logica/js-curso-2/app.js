1b74944348e68ca5bd1d0c36a98a7403fd96d415
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-24 10:54:34 -0300

    commit: verificando se o chute é maior ou menor

--- logica/js-curso-2/app.js
+++ logica/js-curso-2/app.js
@@ -4,9 +4,9 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-let nSecreto = gerarNumeroAleatorio();
+let nSecreto = gerarNumeroAleatorio()
 
-function exibirTexto(tag, texto){
+function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
@@ -14,11 +14,21 @@ function exibirTexto(tag, texto){
 exibirTexto('h1', 'Jogo do Número Secreto');
 exibirTexto('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute(){
+function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == nSecreto);
+
+    if (chute == nSecreto) {
+        exibirTexto('h1', 'Acertou!')
+        exibirTexto('p', 'Você descobriu o número secreto!')
+    } else {
+        if (chute > nSecreto){
+            exibirTexto('p', 'O número secreto é menor...')
+        } else {
+            exibirTexto('p', 'O número secreto é maior...')
+        }
+    }
 }
 
-function gerarNumeroAleatorio(){
-    return parseInt(Math.random()* 10 + 1);
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
