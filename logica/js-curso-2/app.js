847ea11b8ae01a7962a7711088bf9f4bed8dc382
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-24 11:15:19 -0300

    commit: btn reiniciar

--- logica/js-curso-2/app.js
+++ logica/js-curso-2/app.js
@@ -7,13 +7,24 @@
 let nSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTexto('h1', 'Jogo do Número Secreto');
-exibirTexto('p', 'Escolha um número entre 1 e 10');
+function exibirMsgInicial(){
+    exibirTexto('h1', 'Jogo do Número Secreto');
+    exibirTexto('p', 'Escolha um número entre 1 e 10');
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -23,6 +34,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > nSecreto){
             exibirTexto('p', 'O número secreto é menor...');
@@ -30,9 +42,16 @@ function verificarChute() {
             exibirTexto('p', 'O número secreto é maior...');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+function reiniciarJogo(){
+    nSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMsgInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
+
+exibirMsgInicial();
